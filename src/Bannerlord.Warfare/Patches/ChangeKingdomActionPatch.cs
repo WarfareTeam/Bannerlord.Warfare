c4a0cba361dfde5d4b7c66bff8d7aa891f02fc7e
Mike <killeymic@gmail.com>
2024-04-19 17:06:06 -0700

    Fixed an issue preventing the player from leaving a kingdom as a mercenary.

--- src/Bannerlord.Warfare/Patches/ChangeKingdomActionPatch.cs
+++ src/Bannerlord.Warfare/Patches/ChangeKingdomActionPatch.cs
@@ -1,15 +1,16 @@
 ﻿using TaleWorlds.CampaignSystem.Actions;
 
 using HarmonyLib;
+using TaleWorlds.CampaignSystem;
 
 namespace Warfare.Patches
 {
     [HarmonyPatch(typeof(ChangeKingdomAction), "ApplyInternal")]
     public static class ChangeKingdomActionPatch
     {
-        public static bool Prefix(ChangeKingdomAction.ChangeKingdomActionDetail detail)
+        public static bool Prefix(Clan clan, ChangeKingdomAction.ChangeKingdomActionDetail detail)
         {
-            return detail != ChangeKingdomAction.ChangeKingdomActionDetail.LeaveAsMercenary;
+            return clan == null || clan == Clan.PlayerClan || detail != ChangeKingdomAction.ChangeKingdomActionDetail.LeaveAsMercenary;
         }
     }
 }
