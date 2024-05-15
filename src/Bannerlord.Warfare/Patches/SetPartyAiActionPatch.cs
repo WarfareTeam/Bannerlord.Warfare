674fbd9006c42492903967fd1c495f9a8d670d7c
Mike <killeymic@gmail.com>
2024-05-15 15:48:08 -0700

    Added logic preventing armies from besieging a settlement that overpowers them if there is a settlement they could defend instead.

--- src/Bannerlord.Warfare/Patches/SetPartyAiActionPatch.cs
+++ src/Bannerlord.Warfare/Patches/SetPartyAiActionPatch.cs
@@ -6,11 +6,13 @@ using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
+using TaleWorlds.LinQuick;
 
 using HarmonyLib;
 
 namespace Warfare.Patches
 {
+
     [HarmonyPatch(typeof(SetPartyAiAction), "GetActionForBesiegingSettlement")]
     public static class SetPartyAiActionPatch
     {
@@ -18,24 +20,22 @@ namespace Warfare.Patches
 
         public static bool Prefix(MobileParty owner, Settlement settlement)
         {
-            if (!Settings.Current.ModifyArmyBesiegeAI)
+            Army army;
+            if (!Settings.Current.ModifyArmyBesiegeAI || (army = owner.Army) == null || settlement.MapFaction.Fiefs.CountQ() < Settings.Current.ModifyArmyBesiegeAIMinimumFiefs || (owner.Ai.DefaultBehavior == AiBehavior.BesiegeSettlement && owner.TargetSettlement == settlement) || !settlement.IsFortification)
             {
-                return true;
-            }
-            if (owner.Army == null || settlement.MapFaction.Fiefs.Count() < Settings.Current.ModifyArmyBesiegeAIMinimumFiefs || (owner.Ai.DefaultBehavior == AiBehavior.BesiegeSettlement && owner.TargetSettlement == settlement) || !settlement.IsFortification)
-            {
-                //Use vanilla logic
-                //if attacker has no army,
-                //if the target settlements faction has less than 3 fiefs,
-                //if attacker is already traveling to besiege this target,
-                //if the settlement is neither a town or castle
+                //Use vanilla logic if:
+                //1. modify army ai setting is disabled
+                //2. attacker has no army
+                //3. target settlements faction has less than 3 fiefs
+                //4. attacker is already traveling to besiege this target
+                //5. if the settlement is neither a town or castle
                 return true;
             }
             foreach (PartyAiAction action in aiActionQueue.ToList())
             {
                 if (action.Expiration.IsFuture)
                 {
-                    if (action.Army == owner.Army && action.Settlement == settlement)
+                    if (army == action.Army && settlement == action.Settlement)
                     {
                         return false;
                     }
@@ -43,15 +43,34 @@ namespace Warfare.Patches
                 }
                 aiActionQueue.Remove(action);
             }
-            float power = settlement.Parties.Where(x => x.IsActive && !x.IsVillager && !x.IsCaravan).Select(x => x.Party.TotalStrength).Sum();
-            float score = owner.ActualClan.Kingdom.Armies.Where(x => x.ArmyOwner != owner.LeaderHero && (x.LeaderParty.SiegeEvent != null || (x.AIBehavior == Army.AIBehaviorFlags.TravellingToAssignment && x.ArmyType == Army.ArmyTypes.Besieger)) && (Settlement)x.AiBehaviorObject != null && (Settlement)x.AiBehaviorObject == settlement).Select(x => (x.TotalStrength - power) / 1000f).Sum();
-            if (MBRandom.RandomFloat < score)
+            float power = settlement.Parties.WhereQ(x => x.IsActive && !x.IsVillager && !x.IsCaravan).SumQ(x => x.Party.TotalStrength);
+            IEnumerable<float> totalStrength = owner.ActualClan.Kingdom.Armies.WhereQ(x => x.ArmyOwner != owner.LeaderHero && (x.LeaderParty.SiegeEvent != null || (x.AIBehavior == Army.AIBehaviorFlags.TravellingToAssignment && x.ArmyType == Army.ArmyTypes.Besieger)) && (Settlement)x.AiBehaviorObject != null && (Settlement)x.AiBehaviorObject == settlement).SelectQ(x => x.TotalStrength);
+            if (power >= totalStrength.SumQ(x => x))
             {
-                aiActionQueue.Add(new PartyAiAction(owner.Army, settlement, CampaignTime.HoursFromNow(Settings.Current.TimeToPreventArmyBesiegeAIHours)));
+                //Force party attempting to siege to instead defend if one of their kingdoms fiefs are under attack
+                //This needs heavy expansion in the future...
+                IEnumerable<Settlement> settlementsUnderSiege = from x in owner.ActualClan.Kingdom.Settlements where x.SiegeEvent != null orderby x.OwnerClan == owner.ActualClan select x;
+                Settlement settlementToDefend;
+                if (!settlementsUnderSiege.IsEmpty() && (settlementToDefend = settlementsUnderSiege.First()) != null)
+                {
+                    SetPartyAiAction.GetActionForDefendingSettlement(owner, settlementToDefend);
+                    AddToQueue(army, settlement);
+                    return false;
+                }
+            }
+            if (MBRandom.RandomFloat < totalStrength.SumQ(x => (x - power) / 1000f))
+            {
+                AddToQueue(army, settlement);
                 return false;
             }
             return true;
         }
+
+        public static void AddToQueue(Army army, Settlement settlement)
+        {
+            aiActionQueue.Add(new PartyAiAction(army, settlement, CampaignTime.HoursFromNow(Settings.Current.TimeToPreventArmyBesiegeAIHours)));
+        }
+
         internal class PartyAiAction
         {
             public Army Army { get; private set; }
